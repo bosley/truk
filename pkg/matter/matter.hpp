d6eedc04d17728ba7b867db0c2fe174a020f0807
bosley <josh.bosley@proton.me>
2025-12-06 16:22:40 -0500

    Sarting matter now

--- pkg/matter/matter.hpp
+++ pkg/matter/matter.hpp
@@ -2,12 +2,28 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <array>
+#include <string_view>
 
 namespace matter {
 
-template <std::size_t WIDTH> class MatterType {
+/*
+   Each substance of matter (deliniation that makes forms) will be unique
+   they will be unique in ways they are accessed/utilized despite the fact
+   that they may have same width. The SUBSTANCE_ID is a means to aide in
+   contextualizing the data, but it may need futher contextualization for
+   interaction
+*/
+template <std::uint8_t SUBSTANCE_ID, std::size_t WIDTH> class matter_base_c {
+  static_assert(WIDTH >= 1, "WIDTH must be at least 1");
+
 public:
-  MatterType() = default;
+  matter_base_c( ) = default;
+  constexpr std::uint8_t id() const { return SUBSTANCE_ID; }
+
+
+private:
+  std::array<std::uint8_t, WIDTH> _data{0};
 };
 
 } // namespace matter
